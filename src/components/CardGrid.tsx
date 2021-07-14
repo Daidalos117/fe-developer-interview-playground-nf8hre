9364fc6f9030d388281cb70a04c511f0088516aa
Roman <rajchert.roman@gmail.com>
2021-07-14 18:28:47 +0200

    Prettify

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,47 +1,56 @@
-import React, { useState } from 'react';
-import { Card } from 'components/Card';
-import { ChallengeCard } from 'types';
-import { StyledGrid, StyledCenter, StyledButton } from 'components/styled';
+import React, { useState } from "react";
+import { Card } from "components/Card";
+import { ChallengeCard } from "types";
+import { StyledGrid, StyledCenter, StyledButton } from "components/styled";
 
 export interface GridProps {
-    defaultRows?: number;
-    defaultColumns?: number;
-    data: ChallengeCard[];
-    search?: string;
+  defaultRows?: number;
+  defaultColumns?: number;
+  data: ChallengeCard[];
+  search?: string;
 }
 
-export const CardGrid: React.FC<GridProps> = ({ data, defaultColumns = 3, defaultRows = 3, search = 3 }) => {
-    const [rows, setRows] = useState(defaultRows);
-    const [hiddenCards, setHiddenCards] = useState<number[]>([]);
-    const numberOfCards = rows * defaultColumns;
-    const filterFunction = (card: ChallengeCard) => {
-        const hidden = !hiddenCards.includes(card.id);
-        if (!hidden) return false;
-        const condition = search
-            ? card.title.match(new RegExp(String(search), 'i')) ||
-              card.description.match(new RegExp(String(search), 'i'))
-            : true;
+export const CardGrid: React.FC<GridProps> = ({
+  data,
+  defaultColumns = 3,
+  defaultRows = 3,
+  search = 3,
+}) => {
+  const [rows, setRows] = useState(defaultRows);
+  const [hiddenCards, setHiddenCards] = useState<number[]>([]);
+  const numberOfCards = rows * defaultColumns;
+  const filterFunction = (card: ChallengeCard) => {
+    const hidden = !hiddenCards.includes(card.id);
+    if (!hidden) return false;
+    return search
+      ? card.title.match(new RegExp(String(search), "i")) ||
+          card.description.match(new RegExp(String(search), "i"))
+      : true;
+  };
+  const filtered = data.filter(filterFunction);
+  const slicedData = data.slice(0, numberOfCards);
+  const mutatedData = slicedData.filter(filterFunction);
 
-        return condition;
-    };
-    const filtered = data.filter(filterFunction);
-    const slicedData = data.slice(0, numberOfCards);
-    const mutatedData = slicedData.filter(filterFunction);
+  return (
+    <>
+      <StyledGrid columns={defaultColumns}>
+        {mutatedData.map(({ id, ...item }) => (
+          <Card
+            {...item}
+            onHideClick={() => setHiddenCards((cards) => [...cards, id])}
+            key={id}
+          />
+        ))}
+      </StyledGrid>
+      <StyledCenter>
+        {numberOfCards < filtered.length && (
+          <StyledButton onClick={() => setRows(rows + 3)}>
+            Load more
+          </StyledButton>
+        )}
 
-    return (
-        <>
-            <StyledGrid columns={defaultColumns}>
-                {mutatedData.map(({ id, ...item }) => (
-                    <Card {...item} onHideClick={() => setHiddenCards((cards) => [...cards, id])} key={id} />
-                ))}
-            </StyledGrid>
-            <StyledCenter>
-                {numberOfCards < filtered.length ? (
-                    <StyledButton onClick={() => setRows(rows + 3)}>Load more</StyledButton>
-                ) : (
-                    'No more cards to show.'
-                )}
-            </StyledCenter>
-        </>
-    );
+        {filtered.length === 0 && <>No cards to show :(</>}
+      </StyledCenter>
+    </>
+  );
 };
